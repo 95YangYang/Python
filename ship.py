2ad152a23f0ee2113b25e323da17c9b6d11cc5f0
yang <18262625618@163.com>
2021-01-30 13:09:02 +0800

    full screen

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
 	def __init__(self, ai_game):
 		"""初始化飞船并设置其初始位置"""
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		# 加载飞船图像并获取其外接矩阵
@@ -14,6 +15,9 @@ class Ship:
 
 		# 对于每艘新飞船，都将其放在屏幕底部的中央
 		self.rect.midbottom = self.screen_rect.midbottom
+
+		# 在飞船的属性x中存储小数值
+		self.x = float(self.rect.x)
 
 		# 移动标志
 		self.moving_right = False
@@ -24,7 +28,9 @@ class Ship:
 		self.screen.blit(self.image, self.rect)
 
 	def update(self):
-		if self.moving_right:
-			self.rect.x += 1
-		if self.moving_left:
-			self.rect.x -= 1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > self.screen_rect.left:
+			self.x -= self.settings.ship_speed
+
+		self.rect.x = self.x

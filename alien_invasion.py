422606529cbc8eb82e5ef84d52a2c68efbaa3a35
yang <18262625618@163.com>
2021-01-31 10:18:46 +0800

    finish chapter 12

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""管理游戏资源和行为的封装"""
@@ -11,12 +12,14 @@ class AlienInvasion:
 
 		self.settings = Settings()
 		
+		# 设置全屏显示
 		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 		self.settings.screen_width = self.screen.get_rect().width
 		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
 
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 		# 设置背景色
 		self.bg_color = (230, 230, 230)
@@ -27,6 +30,7 @@ class AlienInvasion:
 			# 监视键盘和鼠标事件
 			self._check_events()
 			self.ship.update()
+			self._update_bullets()
 			# 每次循环时都重绘屏幕
 			self._update_screen()
 
@@ -44,8 +48,15 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_RIGHT:
 			self.ship.moving_right = True
-		elif event.key == pygame.K_Q:
+		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
+
+	def _fire_bullet(self):
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
 
 	def _check_keyup_events(self, event):
 		if event.key == pygame.K_LEFT:
@@ -53,9 +64,20 @@ class AlienInvasion:
 		elif event.key == pygame.K_RIGHT:
 			self.ship.moving_right = False
 
+	def _update_bullets(self):
+		"""更新子弹的位置并删除消失的子弹"""
+		self.bullets.update()
+
+		# 删除消失的子弹
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+
 	def _update_screen(self):
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 		pygame.display.flip()
 
